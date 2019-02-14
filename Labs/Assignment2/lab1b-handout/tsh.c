f2c19956fff4f7735d5304b349d29bf1e07fdaed
Joseph M Dunton <jmdunton@wilkes.cs.millersville.edu>
2019-02-14 16:56:19 -0500

    started lab 2

--- Labs/Assignment2/lab1b-handout/tsh.c
+++ Labs/Assignment2/lab1b-handout/tsh.c
@@ -62,7 +62,6 @@ char const* const FG_BG_KILL_NOARG = "%s command requires PID or %%jobid argumen
 char const* const FG_BG_KILL_NOPID = "(%d): No such process\n";
 char const* const FG_BG_KILL_NOJID = "%s: No such job\n";
 char const* const FG_BG_KILL_BADARG = "%s: argument must be a PID or %%jobid\n";
-
 /*
  *******************************************************************************
  * FUNCTION PROTOTYPES
@@ -93,6 +92,21 @@ sigint_handler (int sig);
 void
 sigtstp_handler (int sig);
 
+void
+eval (char *cmdline);
+
+int 
+builtin_command(char **argv);
+
+void
+waitfg (pid_t pid);
+
+pid_t
+Fork(void);
+
+void
+jobs();
+
 /*
  *******************************************************************************
  * MAIN
@@ -112,50 +126,19 @@ main (int argc, char** argv)
   Signal (SIGQUIT, sigquit_handler); /* quit */
 
   /* TODO -- shell goes here */
+  char cmdline[MAXLINE];
 
+  jobctrl_init();
+  while (1){
+	printf("%s",PROMPT);
+	fflush(stdout);
+	fgets(cmdline, MAXLINE, stdin);
+	if(feof(stdin))
+		exit(0);
+	eval(cmdline);
+  }
   /* Quit */
   exit (0);
-}
-
-/*
- *******************************************************************************
- * SIGNAL HANDLERS
- *******************************************************************************
- */
-
-/*
-*  sigchld_handler - The kernel sends a SIGCHLD to the shell whenever
-*      a child job terminates (becomes a zombie), or stops because it
-*      received a SIGSTOP or SIGTSTP signal. The handler reaps all
-*      available zombie children, but doesn't wait for any other
-*      currently running children to terminate.
-*/
-void
-sigchld_handler (int sig)
-{
-
-}
-
-/*
- *  sigint_handler - The kernel sends a SIGINT to the shell whenEver the
- *     user types ctrl-c at the keyboard.  Catch it and send it along
- *     to the foreground job.
- */
-void
-sigint_handler (int sig)
-{
-
-}
-
-/*
- *  sigtstp_handler - The kernel sends a SIGTSTP to the shell whenever
- *      the user types ctrl-z at the keyboard. Catch it and suspend the
- *      foreground job by sending it a SIGTSTP.
- */
-void
-sigtstp_handler (int sig)
-{
-
 }
 
 /*
@@ -279,4 +262,197 @@ parseline (const char* cmdline, char** argv)
     argv[--argc] = NULL;
   }
   return bg;
+}
+/*
+ *******************************************************************************
+ * SIGNAL HANDLERS
+ *******************************************************************************
+ */
+
+/*
+*  sigchld_handler - The kernel sends a SIGCHLD to the shell whenever
+*      a child job terminates (becomes a zombie), or stops because it
+*      received a SIGSTOP or SIGTSTP signal. The handler reaps all
+*      available zombie children, but doesn't wait for any other
+*      currently running children to terminate.
+*/
+void
+sigchld_handler (int sig)
+{
+	pid_t pid;
+	int status;
+	int fg_pid;
+	while((pid = waitpid (-1, &status, WNOHANG|WUNTRACED)) > 0 )
+	{
+		if (WIFSTOPPED(status))
+ 			 {
+				printf("Job (%d) stopped by signal %d\n", (fg_pid = jobctrl_fgpid()), WSTOPSIG(status));
+				fflush(stdout);
+ 			 }
+ 		else if (WIFSIGNALED (status))
+  			 {
+				printf("Job (%d) terminated by signal %d\n", (fg_pid = jobctrl_fgpid()), WTERMSIG(status));
+				fflush(stdout);
+				jobctrl_delete(fg_pid);
+ 			 }
+  		else if (WIFEXITED (status))
+  		 	 {
+				 fg_pid = jobctrl_fgpid();
+				 jobctrl_delete(fg_pid);
+     			 }
+	}
+}
+
+/*
+ *  sigint_handler - The kernel sends a SIGINT to the shell whenEver the
+ *     user types ctrl-c at the keyboard.  Catch it and send it along
+ *     to the foreground job.
+ */
+void
+sigint_handler (int sig)
+{
+  pid_t fg_job;
+  if ((fg_job = jobctrl_fgpid()) != 0)
+  {
+	kill(-fg_job, SIGINT);
+	jobctrl_delete(fg_job);
+  }
+}
+
+/*
+ *  sigtstp_handler - The kernel sends a SIGTSTP to the shell whenever
+ *      the user types ctrl-z at the keyboard. Catch it and suspend the
+ *      foreground job by sending it a SIGTSTP.
+ */
+void
+sigtstp_handler (int sig)
+{
+  pid_t fg_job;
+  if ((fg_job = jobctrl_fgpid()) != 0)
+  {
+	kill(-fg_job, SIGTSTP);
+	jobctrl_delete(fg_job);
+  }
+}
+
+/*
+ *******************************************************************************
+ * HELPER ROUTINES
+ *******************************************************************************
+ */
+
+
+/*
+ * eval - evaluate entered shell commands
+ *
+ * takes *cmdline, the line of shell input to be evaluated.
+ */
+void
+eval (char *cmdline){
+	char *argv[MAXARGS];
+	char buf[MAXLINE];
+	int bg;
+	pid_t pid;
+	sigset_t mask;
+	sigset_t prevMask;
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGCHLD);
+	sigaddset(&mask, SIGTSTP);
+	sigaddset(&mask, SIGINT);
+	sigprocmask(SIG_BLOCK, &mask, &prevMask);
+	strcpy(buf, cmdline);
+	bg = parseline(buf, argv);
+	if (argv[0] == NULL)
+		return;
+	if(!builtin_command(argv)){
+		if ((pid = Fork()) == 0){
+			sigprocmask(SIG_SETMASK, &prevMask, NULL);
+			setpgid(0, 0);
+			if (execve(argv[0], argv, environ) < 0) {
+				printf("%s: Command not found\n", argv[0]);
+				fflush(stdout);
+			}
+		}
+		sigprocmask(SIG_SETMASK, &prevMask, NULL);
+		if (!bg) {
+			waitfg(pid);
+		}
+		else
+		{
+			printf("(%d) %s", pid, cmdline);
+			fflush(stdout);
+		}
+	}
+}
+
+/*
+ * builtin_command - allows features like "quit" and "fg" to be immediatly evaluated.
+ *
+ * takes argv, a potential built-in command
+*/
+int builtin_command(char **argv)
+{
+	pid_t fg_pid;
+
+	if (!strcmp(argv[0], "quit"))
+	{
+		exit(0);
+	}
+	if(!strcmp(argv[0], "fg"))
+	{
+		if((fg_pid = jobctrl_fgpid()) != 0)
+		{
+			kill(-fg_pid, SIGCONT);
+			waitfg(fg_pid);
+		}
+		return 1;
+	}
+	if(!strcmp(argv[0], "jobs"))
+	{
+		jobs();
+		return 1;
+	}
+	else if(!strcmp(argv[0], "&"))
+	{
+		return 1;
+	}
+	return 0;
+}
+
+/*
+ * waitfg - waitfg makes the shell wait for a foreground process to finish, takes in no parameters.
+*/
+void
+waitfg (pid_t pid)
+{
+	pid_t fg_pid;
+	//while runnig & not interrupted, suspend. call sig suspend in a while loop. if running pid is 0, stop waiting.
+	sigset_t mask;
+	sigemptyset(&mask);
+	while((fg_pid = jobctrl_fgpid()) == pid)
+	{
+		sigsuspend(&mask);
+	}
+}
+
+/*
+ *Fork - wrapper class for the fork() function
+*/
+pid_t
+Fork(void)
+{
+	pid_t pid;
+
+	if((pid=fork()) < 0)
+		unix_error("Fork Error");
+	return pid;
+}
+
+/*
+ *Jobs - prints a list of all jobs.
+*/
+void
+jobs()
+{
+	jobctrl_list();
 }
